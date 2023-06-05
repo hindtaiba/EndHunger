f3908dde2311f13f55d3df838163e46924552244
Riwa Hammoud <riwahammoud1@gmail.com>
2023-06-05 23:25:01 +0300

    fix nav of login page

--- app1/urls.py
+++ app1/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 
 urlpatterns = [
- path('', views.home, name='home'),
- path('contact/',views.contact,name='contact'),
- path('register-login/',views.loginRegister,name='loginRegister'),
- path('about/',views.about,name='about')
+    path('', views.home, name='home'),
+    path('contact/',views.contact,name='contact'),
+    path('register-login/',views.loginRegister,name='loginRegister'),
+    path('about/',views.about,name='about')
 ]
